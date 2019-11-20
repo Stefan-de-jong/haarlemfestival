02333a3880874fc8dfbff50903e86f2e6b4769b1
Jim <jimrosenboom@gmail.com>
2019-11-20 19:21:57 +0100

    trying to upload dance info page

---
+++ app/views/pages/dance_info.php
@@ -0,0 +1,44 @@
+<?php
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>hf_dance</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+</head>
+
+<body class="text-center">
+    <section id="bg-image" style="position: absolute;"><img src="assets/img/bg-left.png" style="position: absolute;"><img style="position: absolute;margin-left: 1536px;" src="assets/img/bg-right.png"></section>
+    <section class="text-center" id="welcome-message" style="width: 1154px;background-color: rgb(255,69,69);height: 64px;margin-left: 384px;position: absolute;">
+        <h1>Are you ready to party?</h1>
+    </section>
+    <section id="main-content" style="position: absolute;background-color: rgb(255,104,104);width: 1154px;margin-top: 63px;height: 916px;margin-left: 384px;">
+        <ol class="breadcrumb" style="background-color: rgb(255,255,255);">
+            <li class="breadcrumb-item"><a href="#"><span>Home</span></a></li>
+            <li class="breadcrumb-item"><a href="#"><span>dance information page</span></a></li>
+        </ol>
+        <h4 class="text-center">During Haarlem Festival famous DJs from the Netherlands will perform on various venues.<br>It's a great way to get familiar with the Dutch dance culture that is so famous all aroundthe globe.<br><br></h4>
+        <section><img src="assets/img/artist%20image.png" style="position: absolute;width: 537px;"></section>
+        <section>
+            <h4 class="text-left" style="background-color: rgb(255,62,62);width: 464px;margin-left: 66px;margin-bottom: 0px;">Friday</h4>
+            <h4 class="text-left" style="background-color: rgb(255,255,255);width: 464px;margin-left: 66px;margin-top: 0px;margin-bottom: 0px;">On Friday Nicky Romero, Tiësto, Hardwell, Armin van Buuren and Afrojack will perform.</h4>
+            <section style="background-color: #ffffff;margin-top: 0px;width: 464px;margin-left: 66px;"><button class="btn btn-primary" type="button" style="margin-right: 328px;margin-top: 20px;margin-bottom: 20px;">BUY TICKETS</button></section>
+            <section style="margin-top: 20px;">
+                <h4 class="text-left" style="background-color: rgb(255,62,62);width: 464px;margin-left: 66px;margin-bottom: 0px;">Saturday</h4>
+                <h4 class="text-left" style="background-color: rgb(255,255,255);width: 464px;margin-left: 66px;margin-top: 0px;margin-bottom: 0px;">On Saturday Hardwell, Martin Garrix, Armin van Buuren, Afrojack, Tiësto and Nicky Romoro will perform.</h4>
+                <section style="background-color: #ffffff;margin-top: 0px;width: 464px;margin-left: 66px;"><button class="btn btn-primary" type="button" style="margin-right: 328px;margin-top: 20px;margin-bottom: 20px;">BUY TICKETS</button></section>
+            </section>
+            <section style="margin-top: 20px;">
+                <h4 class="text-left" style="background-color: rgb(255,62,62);width: 464px;margin-left: 66px;margin-bottom: 0px;">Sunday</h4>
+                <h4 class="text-left" style="background-color: rgb(255,255,255);width: 464px;margin-left: 66px;margin-top: 0px;margin-bottom: 0px;">On Sunday Afrojack, Tiësto, Nicky Remero, Armin van Buuren, Hardwell and Martin Garrix will perform.</h4>
+                <section style="background-color: #ffffff;margin-top: 0px;width: 464px;margin-left: 66px;"><button class="btn btn-primary" type="button" style="margin-right: 328px;margin-top: 20px;margin-bottom: 20px;">BUY TICKETS</button></section>
+            </section>
+        </section>
+    </section>
+    <script src="public/js/dance_info.min.js"></script>
+</body>
+
+</html>
+?>
