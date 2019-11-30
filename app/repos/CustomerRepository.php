b831a0a6adf265f85baa08324fb08d1c483fd247
Stefan <sjf.de.jong@gmail.com>
2019-11-30 18:30:32 +0100

    Register customers working
    Edit controller class to be able to work with model objects instead of data objects

--- app/repos/CustomerRepository.php
+++ app/repos/CustomerRepository.php
@@ -6,9 +6,18 @@
             $this->db = new Database;
         }
 
-        public function findId($id){
+        public function findById($id){
             $this->db->query('SELECT * FROM customer WHERE id = :id');
             $this->db->bind(':id', $id);
+
+            $row = $this->db->single();
+
+            return $row;
+        }  
+        
+        public function findByEmail($email){
+            $this->db->query('SELECT * FROM customer WHERE email = :email');
+            $this->db->bind(':email', $email);
 
             $row = $this->db->single();
 
@@ -26,9 +35,10 @@
         }
 
         public function save(Customer $customer){
-            $this->db->query('INSERT INTO customer (name, email, password) VALUES (:name, :email, :password)');
+            $this->db->query('INSERT INTO customer (first_name, last_name, email, password) VALUES (:firstname, :lastname, :email, :password)');
             // Bind values
-            $this->db->bind(':name', $customer->name);
+            $this->db->bind(':firstname', $customer->firstname);
+            $this->db->bind(':lastname', $customer->lastname);
             $this->db->bind(':email', $customer->email);
             $this->db->bind(':password', $customer->password);
 
