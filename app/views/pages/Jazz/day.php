ffbe231e9269313b11982ac2db26a4609e0beabf
Pascal <pascal.put@gmail.com>
2019-12-01 21:48:10 +0100

    pullen om te kijken of alles nog werkt

---
+++ app/views/pages/Jazz/day.php
@@ -0,0 +1,27 @@
+<tr>
+    <td>   
+        <?php echo "date"; ?>
+    </td>
+    <td>   
+        <?php echo "artist"; ?>
+    </td>
+    <td>   
+        <?php echo "location"; ?>
+    </td>
+    <td>   
+        <?php echo "hall"; ?>
+    </td>
+    <td>   
+        <?php echo "time"; ?>
+    </td>
+    <td>   
+        <?php echo "price"; ?>
+    </td>
+    <td>   
+        <input
+		    type="submit"
+		    value="Buy tickets"
+		    class="ChooseTicket"
+	    />
+    </td>
+</tr>
