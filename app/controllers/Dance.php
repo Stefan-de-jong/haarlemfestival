0921de360974bd518bb11920f62a58fbadc53c1d
Jim <jimrosenboom@gmail.com>
2019-12-04 15:14:24 +0100

    little things

---
+++ app/controllers/Dance.php
@@ -0,0 +1,23 @@
+<?php
+class Dance extends Controller{
+        public function __construct(){
+        $this->danceModel= $this->model('Dance');
+        $this->danceRepository = $this->repo('DanceRepository');
+    }
+
+    public function dance_purchase(){
+        $data = [
+            'title' => 'dance'
+        ];
+
+        $this->view('pages/dance/dance_purchase', $data);
+}
+
+public function dance(){
+    $data = [
+        'title' => 'dance'
+    ];
+
+    $this->view('pages/dance/dance_info', $data);
+    }
+}
