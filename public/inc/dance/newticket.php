6d67cf9553e158b9f255c126f3347188be035293
Jim <jimrosenboom@gmail.com>
2019-12-08 17:43:55 +0100

    summary

--- public/inc/dance/newticket.php
+++ public/inc/dance/newticket.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['dance_ticket']))
 $_SESSION['dance_ticket'] = array(); //als er nog geen bestelling is geplaatst maak een array
 }
 $_SESSION['dance_ticket'] = $ticket_info;
-//het idee is om bij de kassa de global variable dance_ticket in te lezen, en daar alle kaartjes uit te halen die voor dance
-//dus van de array worden steeds drie velden uitgelezen, en met deze drie velden kan steeds één ticket naar de database gestuurd worden
+//het idee is om bij de kassa de global variable dance_ticket in te lezen, en daar alle kaartjes uit te halen die voor dance nodig zijn
+//dus van de array worden steeds drie velden uitgelezen, en met deze drie velden kan steeds één bestelling naar de database gestuurd worden
 //aangezien je het event_id, vereiste aantal tickets en de prijs hebt. Het enige dat nog toegevoegd moet worden is email van de klant
 ?>
