e3f1ff3b1bc237fcdc4592d56b1fd351c0dc1734
Jim <jimrosenboom@gmail.com>
2019-12-29 21:36:46 +0100

    Homepage + improved header

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,5 +1,9 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<h1><?php echo $data['title']?></h1>
-<p>Welcome to Haarlem Festival</p>
-
+<section>
+<a href="<?php echo URLROOT;?>/dance/index"><img src="<?php echo URLROOT; ?>/img/homepage/dance.png">
+<a href="<?php echo URLROOT;?>/restaurants/index"><img src="<?php echo URLROOT; ?>/img/homepage/food.png" style="position: absolute;float: right;">
+<a href="<?php echo URLROOT;?>/historic"><img src="<?php echo URLROOT; ?>/img/homepage/history.png">
+<a href="<?php echo URLROOT;?>"><img src="<?php echo URLROOT; ?>/img/homepage/jazz.png" style="position: absolute;float: right;">
+<a href=""></a>
+</section>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
