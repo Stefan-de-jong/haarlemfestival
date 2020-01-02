651e92e3cd40ff67e94494c5871bb8ba8a00c9ff
Jim <jimrosenboom@gmail.com>
2020-01-02 23:20:25 +0100

    Changed comments at search

--- app/views/pages/search.php
+++ app/views/pages/search.php
@@ -1,7 +1,7 @@
 <?php
 //dit bestand gebruikt statische waarde dus als je een event toevoegt of veranderd in de database vergeet hem niet hier ook nog aan te passen!
-//misschien een idee om in de database een apart tabel aan te maken voor zoekresultaten en voor elke entry een string en page op te halen met als primary key een id? Zou veel code schelen.
 //uitleg variabelen: $string is de string die je wilt doorzoeken met $query, de $page is de pagina waar je klanten heen wilt sturen als er zoekresultaten gevonden zijn
+//als je zelf nog zoektermen voor een bepaalde pagina wilt gebruiken kan je die zelf in de string toevoegen. Er staat nu in elk geval tekst die op de pagina zo ie zo staat.
 require_once APPROOT . '/views/inc/header.php';
 if (!isset($search))
 {
