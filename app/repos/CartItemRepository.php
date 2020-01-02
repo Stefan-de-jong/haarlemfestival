719f2aeae7ae5d8b4325a7b1d67fb40e9dbc2bea
Jim <jimrosenboom@gmail.com>
2020-01-02 13:32:15 +0100

    Improved code and ticket generation
    Ticket generation is no longer using ids for All-Access Passes but uses the day at the end of the tickettype (for example, all_access_fri would be the all_access_pass for friday).

--- app/repos/CartItemRepository.php
+++ app/repos/CartItemRepository.php
@@ -59,6 +59,8 @@
         }
 
         public function findDance($id, $amount, $ticket_type){
+            if (strpos($ticket_type, 'dance_ticket') !== false)
+            {
             $this->db->query(
                 'SELECT *,
                             event.id as id                               
@@ -72,14 +74,24 @@
             $this->db->bind(':event_type', 1);
             $this->db->bind(':id', $id);
             $event = $this->db->single();
-
+            }
+            if (strpos($ticket_type, 'dance_ticket') !== false)
+            {
             $this->db->query('SELECT *
             FROM tickettype
             WHERE tickettype.name = :ticket_type
             ');
-            $ticket_type = $ticket_type . "_" . $id;
-            $this->db->bind(':ticket_type', $ticket_type);
+            }
+            else if (strpos($ticket_type, 'all_access') !== false)
+            {
+            $this->db->query('SELECT *
+            FROM tickettype
+            WHERE tickettype.id = :ticket_type
+            ');
+            }
+            $this->db->bind(':ticket_type', $id);
             $ticket = $this->db->single();
+            $ticket_type = $ticket_type . $ticket->name;
             $this->db->query('SELECT * FROM venue
             INNER JOIN danceevent ON danceevent.location = venue.id WHERE danceevent.id = :id'
             );
@@ -90,16 +102,24 @@
             $cartItem = new DanceCartItem($event->id, $event->event_type, $ticket_type, $amount, $event->date, $event->begin_time, $event->name, $ticket->price, $location->name, $location->address);
             }
             else if (strpos($ticket_type, 'all_access') !== false)
+            $stringdate = "";
             {
-            //currently I am checking for static ids but if the id changes in the system this will not work anymore so I will probably implent a way where I don't have to depend on ids later
-            if ($id == 114)
-            {$date = DateTime::createFromFormat('d-m-Y', '27-07-2020');}
-            if ($id == 115)
-            {$date = DateTime::createFromFormat('d-m-Y', '28-07-2020');}
-            if ($id == 116)
-            {$date = DateTime::createFromFormat('d-m-Y', '28-07-2020');}
-            if ($id == 117)
-            {$date = DateTime::createFromFormat('d-m-Y', '00-00-00');}
+            switch(substr($ticket->name, -3))
+            {
+            case 'fri':
+            $stringdate = '27-07-2020';
+            break;
+            case 'sat':
+            $stringdate = '28-07-2020';
+            break;
+            case 'sun';
+            $stringdate = '28-07-2020';
+            break;
+            case 'all';
+            $stringdate = '00-00-00';
+            break;
+            }
+            $date = DateTime::createFromFormat('d-m-Y', $stringdate);
             $time = DateTime::createFromFormat('H:i:s', '00:00:00');
             $cartItem = new DanceCartItem($id, 1, $ticket_type, $amount, $date->format('d-m-Y'), $time->format('H:i:s'), "Multiple Artist", $ticket->price, 0, 0);
             }
