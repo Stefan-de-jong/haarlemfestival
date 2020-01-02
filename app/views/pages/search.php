0eda555c79e08449d9ae5086e488a04e7702a179
Jim <jimrosenboom@gmail.com>
2020-01-02 23:23:30 +0100

    Changed comments yet again

--- app/views/pages/search.php
+++ app/views/pages/search.php
@@ -1,6 +1,5 @@
 <?php
 //uitleg variabelen: $string is de string die je wilt doorzoeken met $query, de $page is de pagina waar je klanten heen wilt sturen als er zoekresultaten gevonden zijn
-//als je zelf nog zoektermen voor een bepaalde pagina wilt gebruiken kan je die zelf in de string toevoegen. Er staat nu in elk geval tekst die op de pagina zo ie zo staat.
 require_once APPROOT . '/views/inc/header.php';
 if (!isset($search))
 {
