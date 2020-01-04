743f3a86e0da44fdb0ea8ada2efe432c51108854
Jim <jimrosenboom@gmail.com>
2020-01-04 21:45:39 +0100

    Discovered a bug in the payment module

--- app/views/pages/cart.php
+++ app/views/pages/cart.php
@@ -80,7 +80,7 @@ require APPROOT . '/views/inc/header.php';
                 </td>
             </tr>
             <tr>
-                <td><?php if(strpos($item->getTicketType(), "all_access") !== false){if (substr($item->getTicketType(), -3) != 'all'){echo date_format($date,"d F Y");}else{echo "No specific date";}} else{echo date_format($time, "H:i") . "<br>". date_format($date,"d F Y");} ?>
+                <td><?php if(strpos($item->getTicketType(), "all_access") !== false){if (substr($item->getTicketType(), 3, -3) == 'all'){echo date_format($date,"d F Y");}else{echo "No specific date";}} else{echo date_format($time, "H:i") . "<br>". date_format($date,"d F Y");} ?>
                 </td>;
                 <td><?php if(strpos($item->getTicketType(), "dance_ticket") !== false){echo $item->getVenue() . "<br>" . $item->getAddress();}?>
                 </td>
