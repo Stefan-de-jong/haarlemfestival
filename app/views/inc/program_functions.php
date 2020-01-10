8d146ad4d02d04e5ecddc0cffa927b357faf7c61
Pascalle <pascalle.schipper@me.com>
2020-01-10 23:04:08 +0100

    Program past dynamisch aan maar bevat bugs, niet AF

---
+++ app/views/inc/program_functions.php
@@ -0,0 +1,136 @@
+<?php
+function fillFoodFavorite($date, $restauarnts)
+{
+    $used_rest = "";
+    $fav_rest_count = 0;
+    $fav_restaurant_ids = array();
+    $tableString = "";
+    $returnResults = array();
+
+    foreach ($restauarnts as $restaurant) {
+        if ($restaurant->getDate() != $date) {continue;}
+        else if ($used_rest == $restaurant->getRestName()) {continue;}
+
+        $tableString = $tableString . "<tr>";
+        $tableString = $tableString . "<td >" . $restaurant->getRestName() . "</td>";
+        for ($i = 1; $i < 16; $i++) {
+            $tableString = $tableString . "<td width='75px'></td>";
+        }
+        $tableString = $tableString . "</tr>";
+        $used_rest = $restaurant->getRestName();
+        $fav_rest_count += 1;
+        array_push($fav_restaurant_ids, $restaurant->getId());
+    }
+    array_push($returnResults, $tableString);
+    array_push($returnResults, $fav_restaurant_ids);
+    array_push($returnResults, $fav_rest_count);
+    return $returnResults;
+}
+
+function fillHistoricFavorites($date, $languages)
+{
+
+}
+
+function fillDanceFavorites($date, $artist)
+{
+
+}
+
+
+function getEvent($events, $date, $time, $id){
+    $eventToShow = null;
+    foreach ($events as $event){
+        switch ($event->getEventType()){
+            case "1";
+                //dance make div
+                break;
+            case "2";
+                if ($event->getDate() == $date && ($event->getBeginTime() == $time.":00:00"|| $event->getBeginTime() == $time.":30:00" ) && $event->getId() == $id){
+                    if( $event->getBeginTime() == $time.":30:00")
+                        return getFoodShowDiv($event, 1);
+                    else
+                        return getFoodShowDiv($event, 0);
+                }
+                break;
+            case "3";
+                //kijken of er een event begint op het tijdstip
+                if ($event->getDate() == $date && $event->getBeginTime() == $time.":00:00" && $event->getLanguageId() == $id){
+                    $eventToShow = getHistShowDiv($event);
+                }
+                break;
+            case "4";
+                //dance make div
+                break;
+        }
+    }
+    return $eventToShow;
+}
+
+function getFoodShowDiv($event, $time_path){
+    //base background kleur maken
+    $bgColor = "white";
+    $duration = 1.5;
+    //restuarant ophalen om de achtergond kleur te kunnen bepalen
+    $restaurant = $event->getId();
+    switch ($restaurant)
+    {
+        case "1":
+            $bgColor = "orange";
+            break;
+        case "2":
+            $duration = 2;
+            $bgColor = "hotpink";
+            break;
+        case "3":
+            $duration = 2;
+            $bgColor = "red";
+            break;
+        case "4":
+            $bgColor = "blue";
+            break;
+        case "5":
+            $bgColor = "black";
+            break;
+        case "6":
+            $bgColor = "green";
+            break;
+        case "7":
+            $bgColor = "brown";
+            break;
+        case "8":
+            $bgColor = "purple";
+            break;
+    }
+    //aanmaken van de div
+    $margin = 0;
+    if($time_path == 1)
+        $margin = 50;
+
+    $eventShow =
+        "<div id='rest_div' style='background-color: " . $bgColor . "; margin-left:".$margin."%; width: ".($duration * 103)."% '>Session: ".$event->getSession()."<br></div>";
+
+    return $eventShow;
+}
+function getHistShowDiv($event){
+    $duration = 1.5;
+    $language = $event->getLanguage();
+    switch ($language){
+        case "Nederlands":
+            $bgColor = "orange";
+            $textColor = "black";
+            break;
+        case "English":
+            $bgColor = "blue";
+            $textColor = "white";
+            break;
+        case "Chinese":
+            $bgColor = "red";
+            $textColor = "white";
+            break;
+    }
+    $margin = 0;
+    $eventShow = "<div id='rest_div' style='background-color: " . $bgColor . "; color: " . $textColor . "; margin-left:".$margin."%; width: ".($duration * 103)."% '>Guide: ".$event->getGuide()."<br></div>";
+    return $eventShow;
+}
+?>
