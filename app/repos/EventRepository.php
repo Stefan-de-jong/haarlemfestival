52350a705607bb9b3e125097c93cf25e95777484
Jim <jimrosenboom@gmail.com>
2020-01-15 00:09:58 +0100

    program fix

--- app/repos/EventRepository.php
+++ app/repos/EventRepository.php
@@ -68,5 +68,23 @@
             }
             return $events;
         }
+
+    public function findAllDanceEvents()
+    {
+    $events = array();
+    $this->db->query('SELECT * FROM event
+    INNER JOIN danceevent ON event.id = danceevent.id
+    INNER JOIN (SELECT * FROM artist as a) a ON a.artist_id = danceevent.artist
+    INNER JOIN (SELECT * FROM venue as v) v ON v.id = danceevent.location
+    INNER JOIN (SELECT * FROM tickettype as t) t on t.id = event.id');
+    $results = $this->db->resultSet();
+    foreach ($results as $result)
+    {
+    $event = new DanceEvent($result->id, $result->date, $result->begin_time, $result->end_time, $result->event_type, $result->n_tickets, $result->price, $result->artist_name, $result->artist_id, $result->venue_name, $result->address);
+    array_push($events, $event);
     }
+    return $events;
+    }
+}
+
 ?>
