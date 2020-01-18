15649c03f6e1a9ad501a12f6c11c21a2638efe79
Jim <jimrosenboom@gmail.com>
2020-01-18 02:35:44 +0100

    Fixed favorite ticket

--- app/repos/TicketRepository.php
+++ app/repos/TicketRepository.php
@@ -99,11 +99,11 @@
                 INNER JOIN (SELECT * FROM tickettype as t) t on t.id = ticket.event_id
                 WHERE ticket.buyer_email = :email');
                 $this->db->bind(':email', $email);
-                $tickets = $this->db->resultSet();
-                foreach ($tickets as $ticket)
+                $sql = $this->db->resultSet();
+                foreach ($sql as $result)
                 {
                                                     //$event_id, $ticket_type, $ticket_price, $buyer_email, $event_type, $date, $time, $price, $venue, $artist, $ticket_name
-                    $ticket = new DanceTicket($ticket->event_id, $ticket->ticket_type, $ticket->ticket_price, $ticket->buyer_email, $ticket->event_type, $ticket->date, $ticket->begin_time, $ticket->ticket_price, $ticket->venue_name, $ticket->artist_name, $ticket->name);
+                    $ticket = new DanceTicket($result->event_id, $result->ticket_type, $result->ticket_price, $result->buyer_email, $result->event_type, $result->date, $result->begin_time, $result->ticket_price, $result->venue_name, $result->artist_name, $result->name);
                     array_push($tickets, $ticket);
                 }
                 return $tickets;
