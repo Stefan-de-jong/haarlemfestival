c39939fce6ed359ad0add5ea4dcd7dde708cdac0
Tom <623381@student.inholland.nl>
2020-11-11 19:35:58 +0100

    init

--- app/config/config.php
+++ app/config/config.php
@@ -1,35 +1,9 @@
 <?php
-    // Database parameters OP HOSTING
-    // define('DB_HOST', 'localhost');
-    // define('DB_USER', 'hfa4_user');
-    // define('DB_PASSWORD', 'Festival123');
-    // define('DB_NAME', 'hfa4_HF');
 
-    // // // Database parameters LOCAL
-    //  define('DB_HOST', 'localhost');
-    //  define('DB_USER', 'root');
-    //  define('DB_PASSWORD', '');
-    //  define('DB_NAME', 'hfa4_HF');
-
-    //Database parameters LOCAL Stefan
     define('DB_HOST', 'localhost');
    define('DB_USER', 'root');
-   define('DB_PASSWORD', '123456');
+   define('DB_PASSWORD', '');
     define('DB_NAME', 'hfa4_HF');
-
-    // //database parameters LOCAL Jim
-    // define('DB_HOST', 'localhost');
-    // define('DB_USER', 'root');
-    // define('DB_PASSWORD', '');
-    // define('DB_NAME', 'hfa4_HF');
-
-    // //database parameters LOCAL Pascalle
-    //   define('DB_HOST', 'localhost');
-    //   define('DB_USER', 'root');
-    //   define('DB_PASSWORD', '');
-    //   define('DB_NAME', 'haarlem_festival_v2');
-    
-
     // App Root
     define('APPROOT', dirname(dirname(__FILE__)));
     // URL Root
